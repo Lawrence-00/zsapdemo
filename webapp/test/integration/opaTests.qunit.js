503bd8cc4946455d033b0abee154cc7c9791601a
Lawrence Chetty <lawrence-sammanasu.chetty@capgemini.com>
2024-07-22 11:53:27 +0000

    First fiori app

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["zsapdemo/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zsapdemo/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
